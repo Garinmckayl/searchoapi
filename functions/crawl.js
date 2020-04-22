4b2fc984d43646d86ed9cc2a14fbc4573487d878
Natnael Getenew <natnaelgetenew@gmail.com>
2020-04-22 09:57:25 +0000

    Initial commit

---
+++ functions/crawl.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+import cheerio from "cheerio";
+
+
+
+exports.handler = async (event, context) => {
+  // Only allow POST
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  }
+
+
+
+
+  const url = 'https://news.ycombinator.com';
+
+axios.get(url)
+  .then(response => {
+    let html = response.data;
+    console.log(getData(html));
+  })
+  .catch(error => {
+    console.log(error);
+  })
+
+let getData = html => {
+  data = [];
+  const $ = cheerio.load(html);
+  $('table.itemlist tr td:nth-child(3)').each((i, elem) => {
+    data.push({
+      title : $(elem).text(),
+      link : $(elem).find('a.storylink').attr('href')
+    });
+  });
+  return data;
+
+
+    .then(() => ({
+      statusCode: 200,
+      body: `Hello, ${name}! Your greeting has been sent to Slack 👋`
+    }))
+    .catch(error => ({
+      statusCode: 422,
+      body: `Oops! Something went wrong. ${error}`
+    }));
+
+}
